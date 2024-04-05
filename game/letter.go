94e6ae772f5bf808136e4ef473fbf69133521d52
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2024-04-05 18:31:53 +0300

    refactor(game): remove unused code

--- game/letter.go
+++ game/letter.go
@@ -2,8 +2,6 @@ package game
 
 import (
 	"fmt"
-
-	"github.com/bigmate/slice"
 )
 
 type Letter interface{}
@@ -24,20 +22,4 @@ func (l PresentLetter) String() string {
 
 func (l ExactLetter) String() string {
 	return fmt.Sprintf("(! %c)", rune(l))
-}
-
-func presents(ls []Letter) []PresentLetter {
-	return slice.Map(
-		slice.Filter(ls, isPresentLetter),
-		func(l Letter) PresentLetter { return l.(PresentLetter) },
-	)
-}
-
-func isPresentLetter(l Letter) bool {
-	switch l.(type) {
-	case PresentLetter:
-		return true
-	default:
-		return false
-	}
 }
